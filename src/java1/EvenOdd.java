d5b2f921889026d63843b8ca49e0eade2a2e97c1
kayoung.kim <kk0mtl@naver.com>
2024-03-21 13:44:26 +0900

    EvenOdd 추가

---
+++ src/java1/EvenOdd.java
@@ -0,0 +1,18 @@
+package java1;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 입력하시오: ");
+		int n = sc.nextInt();
+		
+		if(n % 2 ==0) System.out.println("입력된 정수는 짝수입니다.");
+		else System.out.println("입력된 정수는 홀수입니다.");
+		
+		sc.close();
+	}
+
+}
